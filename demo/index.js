a5dbf416a062fa7ffaf577eca8367793abaceb7b
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-10-08 14:35:59 +0200

    Blank demo.

---
+++ demo/index.js
@@ -0,0 +1,13 @@
+const ezp = require('ezp');
+
+/*
+
+const P = ezp({
+    // get: function(props) { ... }
+    // set: function(props, value) { ... }
+    // delete: function(props) { ... }
+    // has: function(props, value) { ... }
+    // apply: function(props, args) { ... }
+}, '$');
+
+*/
